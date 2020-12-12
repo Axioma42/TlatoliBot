8e1fee4b1fd5e31973c2637e3dc04bdefcd5817f
ThaddeusPaez <thaddeus.paez@gmail.com>
2020-12-12 12:01:24 -0600

    correccion de conexion

--- Sección_Introducción_Frases/php/datosConexion.php
+++ Sección_Introducción_Frases/php/datosConexion.php
@@ -1,6 +1,6 @@
 <?php
 //if(!($link = mysqli_connect("tlatolibot.com", "tlatolib_user", "TlaU$er1", "tlatolib_db")))
-$db_host = "tlatolibot.com";
+$db_host = "localhost";
 $db_name = "tlatolib_db";
 $db_user = "tlatolib_user";
 $db_pass = "TlaU$er1";
