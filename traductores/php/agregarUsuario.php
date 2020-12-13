2b7294b9cad5cec1f64b15f49c26ee90f84829c8
ThaddeusPaez <thaddeus.paez@gmail.com>
2020-12-12 19:41:28 -0600

    concluye verificación de correo para registro de usuarios

--- traductores/php/agregarUsuario.php
+++ traductores/php/agregarUsuario.php
@@ -29,25 +29,62 @@
 			echo '<script>alert("Las contraseñas no coinciden")
                 self.location = "../login/register.html"</script>';
 		else {
+			//Genera codigo de validacion
+			$validation_code = md5(strval(rand()));
 			//Hash del password
 			$pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 			$name = $_POST['name'];
         	$last = $_POST['last'];
 			$lang = $_POST['lang'];
-			$sentencia = "INSERT INTO Users (username, pass, name, last, lang, join_date) VALUES ('".$mail."', '".$pass."', '".$name."', '".$last."', '".$lang."', CURRENT_TIMESTAMP())";
+			$sentencia = "INSERT INTO Users_temp (username, pass, name, last, lang, join_date, validation_code) VALUES ('".$mail."', '".$pass."', '".$name."', '".$last."', '".$lang."', CURRENT_TIMESTAMP(), '".$validation_code."')";
 			$resultado = mysqli_query($link, $sentencia); 
     		if(!$resultado)
         		die("Error en la sentencia ".mysqli_error($link));
 			else {
+				//Envía liga por correo
 				mysqli_close($link);
-				$_SESSION['logged'] = TRUE;
-				$_SESSION['user']=$username;
-				$_SESSION['mail']=$mail;
-        		$_SESSION['name']=$name;
-        		$_SESSION['last']=$last;
-				$_SESSION['phone']=$phone;
-				echo '<script>alert("Registro exitoso")
-                self.location = "../index.html"</script>';
+		$origenNombre = 'Tlatolibot.com'; //nombre que visualiza el receptor del email como "origen" del email (es quien envía el email)
+		$origenEmail = 'noreply@tlatolibot.com';//email que visualiza el receptor del email como "origen" del email (es quien envía el email)
+		$destinatarioEmail = $mail; //destinatario del email, o sea, a quien le estamos enviando el email
+		$destinatarioNombre = $name." ".$last;
+
+		$uid = md5(uniqid(time())); //fabrico un ID único que usaré para el "boundary"
+
+		$asuntoEmail = 'Verificación de correo electrónico - Tlatolibot'; //asunto del email
+
+		//cuerpo del email:
+		$cuerpoMensaje = "Nombre: ".$destinatarioNombre."\r\n";
+		$cuerpoMensaje .= "Correo: ".$destinatarioEmail."\r\n";
+		$cuerpoMensaje .= "Ingrese a la siguiente liga o cópiela en su navegador y cambie su contraseña:\r\n";
+		$cuerpoMensaje .= "https://www.tlatolibot.com/traductores/php/validar_mail.php?validation_code=".$validation_code."\r\n";
+		$cuerpoMensaje .= "Favor de no responder a este correo.";
+		//fin cuerpo del email.
+
+		//cabecera del email (forma correcta de codificarla)
+		$header = "From: " . $origenNombre . " <" . $origenEmail . ">\r\n";
+		$header .= "Reply-To: " . $origenEmail . "\r\n";
+		$header .= "MIME-Version: 1.0\r\n";
+		$header .= "Content-Type: multipart/mixed; boundary=\"" . $uid . "\"\r\n\r\n";
+		//armado del mensaje y attachment
+		$mensaje = "--" . $uid . "\r\n";
+		$mensaje .= "Content-type:text/plain; charset=utf-8\r\n";
+		$mensaje .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+		$mensaje .= $cuerpoMensaje . "\r\n\r\n";
+		$mensaje .= "--" . $uid . "\r\n";
+		//$mensaje .= "Content-Type: application/octet-stream; name=\"" . $archivoNombre . "\"\r\n";
+		//$mensaje .= "Content-Transfer-Encoding: base64\r\n";
+		//$mensaje .= "Content-Disposition: attachment; filename=\"" . $archivoNombre . "\"\r\n\r\n";
+		//$mensaje .= $archivo . "\r\n\r\n";
+		//$mensaje .= "--" . $uid . "--";
+		//envio el email y verifico la respuesta de la función "email" (true o false)
+		if (mail($destinatarioEmail, $asuntoEmail, $mensaje, $header)) {
+			echo '<script>alert("Se le ha enviado un correo electrónico\nRevise su bandeja de entrada")
+					self.location = "../login/login.html"</script>';
+		} else {
+			echo '<script>alert("Se ha producido un error\nIntente más tarde")
+					self.location = "../login/register.html"</script>';
+		}
+
 				}
     		}
 		}
---
+++ traductores/php/validar_mail.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'GET') { //solo ingreso a este bloque de código si el método con el que solicita la página es GET
+	$validation_code = $_GET['validation_code'];
+	require("datosConexion.php");
+    //Conexión a la BD
+	if(!($link = mysqli_connect($db_host, $db_user, $db_pass, $db_name)))
+        printf("Error: %s", mysqli_connect_error());
+    mysqli_query($link, "SET NAMES 'utf8'");
+	$sentencia = "SELECT * FROM Users_temp WHERE validation_code='".$validation_code."'";
+	$resultado = mysqli_query($link, $sentencia); 
+	if(!$resultado) 
+        die("Error en la sentencia ".mysqli_error($link));
+
+    $fila = mysqli_fetch_array($resultado);
+	//Resultado nulo -> no existe el codigo
+    if(mysqli_num_rows($resultado)==0){
+		mysqli_close($link);
+        echo '<script>alert("La liga de validación ha expirado.")
+                self.location = "../login/register.html"</script>';
+	}
+	//Resultado no nulo -> registra usuario nuevo
+    else {
+		$_SESSION['logged']=TRUE;
+		$_SESSION['username']=$fila['username'];
+		$_SESSION['name']=$fila['name'];
+		$_SESSION['last']=$fila['last'];
+		$_SESSION['lang']=$fila['lang'];
+		$_SESSION['join_date']=$fila['join_date'];
+		
+		$sentencia = "INSERT INTO Users (username, pass, name, last, lang, join_date, forgot_code) VALUES ('".$fila['username']."', '".$fila['pass']."', '".$fila['name']."', '".$fila['last']."', '".$fila['lang']."', '".fila['join_date']."', NULL)";
+		$resultado = mysqli_query($link, $sentencia); 
+    	if(!$resultado)
+        	die("Error en la sentencia ".mysqli_error($link));
+		$sentencia = "DELETE FROM Users_temp WHERE Users_temp.validation_code = '".$validation_code."'";
+		$resultado = mysqli_query($link, $sentencia); 
+    	if(!$resultado)
+        	die("Error en la sentencia ".mysqli_error($link));
+		mysqli_close($link);
+		
+		echo '<script>alert("Registro completado y correo validado.")
+                self.location = "../index.html"</script>';
+	}
+}
+
+?>
